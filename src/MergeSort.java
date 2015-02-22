3937011cb11ab6e0f157c5191d7e047f4a92adb7
Karishma <tikkysood@yahoo.com>
2015-02-21 18:23:29 -0700

    Update MergeSort.java

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,41 +1,34 @@
 import java.io.*;
 import java.util.*;
-
 public class MergeSort {
-  
   /**
    * main program
    * @param args
    * @throws IOException
    */
   public static void main(String[] args) throws IOException {
-    
     ArrayList<String> words = getWords();
     ArrayList<String> sortedWords = mergeSort(words);
     int interval = 100;
-    String[] find = new String[100];    
+    String[] find = new String[100];
     int index = interval;
     for (int i = 0; i < interval - 1; i++) {
       index += sortedWords.size() / interval;
       find[i] = sortedWords.get(index);
     }
     find[interval - 1] = "Xdfsdda";
-    
     SearchResult seqResult;
     SearchResult binResult;
-    
     System.out.println("#\tBinary\tSequential\tindex\tword");
-    
     for (int i = 0; i < find.length; i++) {
       seqResult = sequentialSearch(sortedWords, find[i]);
       binResult = binarySearch(sortedWords, find[i]);
       System.out.println(i + "\t" + binResult.getIterations() + "\t" + seqResult.getIterations() + "\t" + binResult.getIndex() + ":" + seqResult.getIndex() + "\t" + find[i] );
     }
   }
-  
   /**
    * sequential search to find wordToFind in the ArrayList words
-   * 
+   *
    * @param words - List of words
    * @param wordToFind - String to find in words
    * @return a SearchResult (index of item found or -1 if not found, number of iterations in search loop)
@@ -48,12 +41,11 @@ public class MergeSort {
         return new SearchResult(i, iterations);
       }
     }
-    return new SearchResult(-1, iterations);  
+    return new SearchResult(-1, iterations);
   }
-  
   /**
    * binary search to find wordToFind in the ArrayList words
-   * 
+   *
    * @param words - List of words
    * @param wordToFind - String to find in words
    * @return a SearchResult (index of item found or -1 if not found, number of iterations in search loop)
@@ -74,43 +66,53 @@ public class MergeSort {
         max = mid - 1;
       }
     }
-    return new SearchResult(-1, iterations);  
+    return new SearchResult(-1, iterations);
   }
-  
   /**
    * Use merge sort to recursively sort the list passed in.
    * @param list - ArrayList of words to be sorted
    * @return A list representing a sorted version of list that was passed in.
    */
   public static ArrayList<String> mergeSort(ArrayList<String> list) {
-    if(list.size==1)
-    {
-    }
+    if(list.size()<=1)
+    {return list;}
     else
     {
-      ArrayList <String> left=list.copyOfRange(list,0, list.size/2);
-      ArrayList <String> right=list.copyOfRange(list,list.size/2,list.size);
-      mergeSort(righta);
+      ArrayList<String> left= new ArrayList<String> (list.subList(0, list.size()/2));
+      ArrayList<String> right=new ArrayList<String> (list.subList(list.size()/2, list.size()));
       mergeSort(left);
-      merge(list, left, right);
-      return list;
+      mergeSort(right);
+      return merge(list, left, right);
     }
   }
-  
   /**
    * Merge the left and right lists together in sorted order.
-   * 
+   *
    * @param leftList The first list to be merged.
    * @param rightList The second list to be merged.
    * @return A single list by merging the two parameters in sorted order.
    */
-  private static ArrayList<String> merge(ArrayList<String> leftList, ArrayList<String> rightList) {
-    
+  private static ArrayList<String> merge(ArrayList<String> result,ArrayList<String> leftList, ArrayList<String> rightList) {
+    int a=0;
+    int b=0;
+    for(int i=0; i<result.size(); i++)
+    {
+      if(b>=rightList.size() || (a<leftList.size() && (leftList.get(a).compareTo(rightList.get(b))<0)))
+      {
+        result.set(i, leftList.get(a));
+        b++;
+      }
+      else
+      {
+        result.set(i, rightList.get(b));
+        b++;
+      }
+    }
+    return result;
   }
-  
   /**
    * create an ArrayList<String> and populate it from text file
-   * 
+   *
    * @return an ArrayList<String>
    * @throws FileNotFoundException
    */
